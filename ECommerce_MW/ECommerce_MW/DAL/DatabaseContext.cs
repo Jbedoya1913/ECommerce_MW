edd0634ea6e605b7631b39fc7643ce4a210059a0
Julian Bedoya <julianb1913@gmail.com>
2023-03-14 20:54:13 -0500

    Create Dababase ECommerce

---
+++ ECommerce_MW/ECommerce_MW/DAL/DatabaseContext.cs
@@ -0,0 +1,27 @@
+﻿using ECommerce_MW.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+using System.Reflection.Emit;
+
+namespace ECommerce_MW.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+             
+        }
+
+        public DbSet<Country> Countries { get; set; }
+      
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+         
+        }
+    }
+    
+}
---
+++ ECommerce_MW/ECommerce_MW/DAL/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ECommerce_MW.DAL.Entities
+{
+    public class Country : Entity
+    {
+        [Display(Name = "País")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe ser de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es oblilgatorio.")]
+        public string Name { get; set; }
+    }
+}
+
